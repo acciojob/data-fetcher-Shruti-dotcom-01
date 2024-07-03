1054210c04fb58e2dae367ee06f13715b6b264fa
shruti <shrutimsvaidya@gmail.com>
2024-07-03 15:55:35 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+const api = " https://dummyjson.com/products";
+
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const[data, setdata] = useState(null);
+  const[isLoading, setIsLoading] = useState(false);
+  const[isError, setIsError] = useState(false);
+
+  useEffect(()=> {
+    setIsError(false);
+    setIsLoading(true);
+    fetch(api)
+     .then((res)=> res.json())
+     .then((d) => {
+       setData(d);
+       setIsLoading(false);
+    })
+    .catch((e) => setIsError(e.message));
+}, []);
+
+return (
+  <div>
+    <h1>Data Fetched from API</h1>
+    {isLoading ? <p>Loading...</p> : <p>An error occurred: {isError}</p>}
+    <pre>{data && JSON.stringify(data)}</pre>
+  </div>
+);
+};
+
 
 export default App
