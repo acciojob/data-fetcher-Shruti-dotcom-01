edf0605a4caeae9c9f508d478a236e6b24ab3d22
shruti <shrutimsvaidya@gmail.com>
2024-07-03 16:17:41 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,44 @@
-
 import React, { useEffect, useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
-const api = " https://dummyjson.com/products";
+const api = "https://dummyjson.com/products";
 
 const App = () => {
-  const[data, setdata] = useState(null);
-  const[isLoading, setIsLoading] = useState(false);
-  const[isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(()=> {
+  useEffect(() => {
     setIsError(false);
     setIsLoading(true);
     fetch(api)
-     .then((res)=> res.json())
-     .then((d) => {
-       setData(d);
-       setIsLoading(false);
-    })
-    .catch((e) => setIsError(e.message));
-}, []);
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((d) => {
+        setData(d);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsError(true);
+        setErrorMessage(e.message);
+        setIsLoading(false);
+      });
+  }, []);
 
-return (
-  <div>
-    <h1>Data Fetched from API</h1>
-    {isLoading ? <p>Loading...</p> : <p>An error occurred: {isError}</p>}
-    <pre>{data && JSON.stringify(data)}</pre>
-  </div>
-);
+  return (
+    <div>
+      <h1>Data Fetched from API</h1>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>An error occurred: {errorMessage}</p>}
+      {!isLoading && !isError && data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {!isLoading && !isError && !data && <p>No data found</p>}
+    </div>
+  );
 };
 
-
-export default App
+export default App;
